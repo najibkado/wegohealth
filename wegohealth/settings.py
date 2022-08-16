de51d519f6dbf8f301173fc9f50280cd69693368
najibkado <dopeland3@gmail.com>
2022-08-16 01:18:07 +0100

    Added static root

--- wegohealth/settings.py
+++ wegohealth/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
